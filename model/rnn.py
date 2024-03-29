02d65026b8b56d16c2a1c3d59e032537f0fca506
pjtoral-trjn <pjtoral@usc.edu>
2024-03-29 16:40:59 -0700

    removing dropout

--- model/rnn.py
+++ model/rnn.py
@@ -59,11 +59,11 @@ def decision_network(args):
     """
     decision_network_input = tf.keras.Input((args.sequence_length, 256), batch_size=args.batch_size)
     x = tf.keras.layers.Dense(units=128, activation="relu")(decision_network_input)
-    x = tf.keras.layers.Dropout(0.2)(x)
+    # x = tf.keras.layers.Dropout(0.25)(x)
     x = tf.keras.layers.Dense(units=64, activation="relu")(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
+    # x = tf.keras.layers.Dropout(0.25)(x)
     x = tf.keras.layers.Dense(units=32, activation="relu")(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
+    # x = tf.keras.layers.Dropout(0.25)(x)
 
     if args.task_selection == "binary_classification":
         output = tf.keras.layers.Dense(units=1, name="Binary-Classifier", activation="sigmoid")(x)
