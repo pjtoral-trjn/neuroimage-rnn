4da2f5b2944984b4019af681bb9f5d273b6e1562
pjtoral-trjn <pjtoral@usc.edu>
2024-04-03 13:01:38 -0700

    Fixing CNNData.py

--- data/CNNData.py
+++ data/CNNData.py
@@ -146,6 +146,4 @@ class CNNData:
         def __getitem__(self, idx):
             batch_x = self.image_filenames[idx * self.batch_size: (idx + 1) * self.batch_size]
             batch_y = self.labels[idx * self.batch_size: (idx + 1) * self.batch_size]
-            if self.args.task_selection == Constants.multi_classification:
-                return (np.asarray([self.read_scan(path) for path in batch_x]), np.asarray(batch_y[0]).reshape((1,3)))
             return (np.asarray([self.read_scan(path) for path in batch_x]), np.asarray(batch_y))
